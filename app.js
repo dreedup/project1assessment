1dafb4de2c53be0f0675ff06b9eec16b4bbd819b
dreedup <andredransom@gmail.com>
2023-07-10 19:14:41 -0400

    project 1 assess

---
+++ app.js
@@ -0,0 +1,34 @@
+let counter = 1;
+let counterTitle = 0;
+
+const counterValue = document.getElementById('counter-value');
+const incrementBtn = document.getElementById('increment-btn');
+const decrementBtn = document.getElementById('decrement-btn');
+const resetBtn = document.querySelector('#reset');
+
+// To increment the value of counter
+incrementBtn.addEventListener('click', () => {
+    counter++;
+    counterTitle++;
+    counterValue.innerHTML = counter;
+    counterTitleValue.innerHTML = counterTitle;
+});
+  
+// To decrement the value of counter
+decrementBtn.addEventListener('click', () => {
+    counter--;
+    counterTitle--;
+    counterValue.innerHTML = counter;
+    counterTitleValue.innerHTML = counterTitle;
+    
+});
+  
+// To reset the counter to zero
+resetBtn.addEventListener('click', reset);
+  
+function reset() {
+    counter = 0;
+    countertitle = 0;
+    counterValue.innerHTML = counter;
+    counterTitleValue.innerHTML = counterTitle;
+}
