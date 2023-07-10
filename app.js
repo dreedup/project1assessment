fe3d0269c22a2dd94ebd45f790b04c85e100d55e
dreedup <andredransom@gmail.com>
2023-07-10 19:25:20 -0400

    .

--- app.js
+++ app.js
@@ -7,15 +7,13 @@ const incrementBtn = document.getElementById('increment-btn');
 const decrementBtn = document.getElementById('decrement-btn');
 const resetBtn = document.querySelector('#reset');
 
-// To increment the value of counter
 incrementBtn.addEventListener('click', () => {
     counter++;
     counterTitle++;
     counterValue.innerHTML = counter;
     counterTitleValue.innerHTML = counterTitle;
 });
-  
-// To decrement the value of counter
+
 decrementBtn.addEventListener('click', () => {
     counter--;
     counterTitle--;
@@ -23,8 +21,7 @@ decrementBtn.addEventListener('click', () => {
     counterTitleValue.innerHTML = counterTitle;
     
 });
-  
-// To reset the counter to zero
+
 resetBtn.addEventListener('click', reset);
   
 function reset() {
